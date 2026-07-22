6e3722d95e1ae0b424256be6d7b35e9f765ed7b1
shibashis dey <shibashis.dey5@gmail.com>
2026-07-22 19:58:30 +0530

    feat(exception): handle user domain exceptions

--- incidentops/src/main/java/com/incidentops/incidentops/exception/GlobalExceptionHandler.java
+++ incidentops/src/main/java/com/incidentops/incidentops/exception/GlobalExceptionHandler.java
@@ -9,6 +9,34 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+	@ExceptionHandler(UserAlreadyExistsException.class)
+	public ResponseEntity<ApiResponse<Void>> handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
+
+		ApiResponse<Void> response = ApiResponse.<Void>builder()
+				.success(false)
+				.message(ex.getMessage())
+				.data(null)
+				.build();
+
+		return ResponseEntity
+				.status(HttpStatus.CONFLICT)
+				.body(response);
+	}
+
+	@ExceptionHandler(UserNotFoundException.class)
+	public ResponseEntity<ApiResponse<Void>> handleUserNotFoundException(UserNotFoundException ex) {
+
+		ApiResponse<Void> response = ApiResponse.<Void>builder()
+				.success(false)
+				.message(ex.getMessage())
+				.data(null)
+				.build();
+
+		return ResponseEntity
+				.status(HttpStatus.NOT_FOUND)
+				.body(response);
+	}
+
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<ApiResponse<Void>> handleException(Exception ex) {
 
